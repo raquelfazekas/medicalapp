bc1e5395b729753d7b98b8c6d36322cb78b21da0
VictorFazekas <victorfazekas3@gmail.com>
2025-05-25 00:05:01 -0300

    fixing route

--- src/app/api/pacientes/[id]/route.ts
+++ src/app/api/pacientes/[id]/route.ts
@@ -1,9 +1,12 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
-export async function GET(req: Request, { params }: { params: { id: string } }) {
+export async function GET(req: NextRequest, context: { params: { id: string } }) {
+  const { id } = context.params;
+
   const paciente = await prisma.paciente.findUnique({
-    where: { id: params.id },
+    where: { id },
   });
 
   if (!paciente) return NextResponse.json({ error: "Not found" }, { status: 404 });
@@ -11,24 +14,23 @@ export async function GET(req: Request, { params }: { params: { id: string } }) 
   return NextResponse.json(paciente);
 }
 
-export async function PUT(req: Request, { params }: { params: { id: string } }) {
+export async function PUT(req: NextRequest, context: { params: { id: string } }) {
+  const { id } = context.params;
   const data = await req.json();
 
   const paciente = await prisma.paciente.update({
-    where: { id: params.id },
-    data: {
-      nome: data.nome,
-      telefone: data.telefone,
-      email: data.email,
-    },
+    where: { id },
+    data
   });
 
   return NextResponse.json(paciente);
 }
 
-export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+export async function DELETE(req: NextRequest, context: { params: { id: string } }) {
+  const { id } = context.params;
+
   await prisma.paciente.delete({
-    where: { id: params.id },
+    where: { id },
   });
 
   return NextResponse.json({ message: "Paciente deletado" });
