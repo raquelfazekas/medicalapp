1b4640d5da0e3c3f99cfbe8b8801ea12ff418d17
VictorFazekas <victorfazekas3@gmail.com>
2025-06-01 01:58:56 -0300

    fixing last thing

--- src/app/(private)/dashboard/consultas/page.tsx
+++ src/app/(private)/dashboard/consultas/page.tsx
@@ -1,38 +1,9 @@
-"use client";
+import React from 'react'
 
-import { useState } from "react";
-import { Textarea } from "@/components/ui/textarea";
-import PDF2 from "@/components/pdfs/ReceitaEspecial";
-
-function PageConsulta() {
-  const [nome, setNome] = useState("");
-
-  const paciente = "Paciente teste";
-  const endereco = "Rua teste dos testes";
-
+function page() {
   return (
-    <div className="flex gap-4">
-      <div className="w-1/2 p-4 border">
-        <h2 className="text-lg font-bold mb-2">Form</h2>
-        <Textarea
-          value={nome}
-          onChange={(e) => setNome(e.target.value)}
-          placeholder="Digite o nome"
-          className="w-full p-2 border rounded"
-        />
-      </div>
-
-      <div className="w-1/2 p-4 border">
-        <PDF2
-          text={nome}
-          paciente={paciente}
-          endereco={endereco}
-          sexo={"Masculino"}
-          dataEmissao="31/05/2025"
-        />
-      </div>
-    </div>
-  );
+    <div>page</div>
+  )
 }
 
-export default PageConsulta;
+export default page
